1f1035055f34d7debd1fce580b7ac8b0f234d67e
Julien RICHARD <julien.y.richard@gmail.com>
2025-03-27 10:16:12 +0100

    Integratin de apollo client, config, mise en place de useQuery, du schema et typage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,62 @@
+import { useQuery } from "@apollo/client";
 import "./App.css";
+import { gql } from "@apollo/client";
 
+// const request = {
+//   query:
+//     "query GetAllWines {\n  getAllWines {\n    id\n    name\n    region\n    fruit\n    grape_varieties\n  }\n}\n\n\n\n\n",
+//   variables: {},
+//   operationName: "GetAllWines",
+// };
+
+const GET_ALL_WINES = gql`
+  query GetAllWines {
+    getAllWines {
+      id
+      name
+      region
+      fruit
+      grape_varieties
+    }
+  }
+`;
+
+type Wines = {
+  getAllWines: {
+    id: number;
+    name: string;
+    region: string;
+    fruit: string;
+    grape_varieties: string[];
+  }[];
+};
 function App() {
+  // const fetchAllWines = async () => {
+  //   try {
+  //     const result = await fetch("http://localhost:5500", {
+  //       method: "POST",
+  //       body: JSON.stringify(request),
+  //     });
+  //     console.log(result);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+  // useEffect(() => {
+  //   fetchAllWines();
+  // }, []);
+
+  const { loading, error, data } = useQuery<Wines>(GET_ALL_WINES);
+  console.log(data);
+
+  if (error) return <p>Error</p>;
+  if (loading) return <p>Loading</p>;
   return (
     <>
       <h1>Hello Toto</h1>
+      {data?.getAllWines.map((wine) => (
+        <h2>{wine.name}</h2>
+      ))}
     </>
   );
 }
